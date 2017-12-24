cb3ef93152e4dd37369affede1e51312f19ddf0d
siecola <siecola@gmail.com>
2017-12-24 18:51:38 -0200

    First commit. Base project.

---
+++ src/test/java/br/com/siecola/gae_exemplo1/GaeExemplo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.siecola.gae_exemplo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GaeExemplo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
