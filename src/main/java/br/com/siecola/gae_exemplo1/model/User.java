f50cecfd9ada68ca9610c185b5f7ef82275d3af8
siecola <siecola@gmail.com>
2018-02-04 10:00:22 -0200

    Adding user repository.

---
+++ src/main/java/br/com/siecola/gae_exemplo1/model/User.java
@@ -0,0 +1,117 @@
+package br.com.siecola.gae_exemplo1.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+
+public class User implements Serializable, UserDetails {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String gcmRegId;
+
+    private Date lastLogin;
+
+    private Date lastGCMRegister;
+
+    private String role;
+
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGcmRegId() {
+        return gcmRegId;
+    }
+
+    public void setGcmRegId(String gcmRegId) {
+        this.gcmRegId = gcmRegId;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Date getLastGCMRegister() {
+        return lastGCMRegister;
+    }
+
+    public void setLastGCMRegister(Date lastGCMRegister) {
+        this.lastGCMRegister = lastGCMRegister;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
